4cd6e883fa8574ce48e2293b5f7e6135a7fb50a6
LJ Li <ljli@xrex.io>
2023-04-20 12:17:26 +0800

    feat: renew with web3

--- contract/dto/contract_basic.py
+++ contract/dto/contract_basic.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel
+from web3.contract import Contract
 
 from contract.dto.chain import Chain
 
 
 class ContractBasicParams(BaseModel):
     """contract basic params"""
-    address: str
-    abi: dict
-    bytecode: str
-    bytecode_runtime: str
-    protocol_name: str
-    chain: Chain
+    protocol_name: str  # 用於區分不同的 protocol 的 contract
+    chain: Chain  # web3.py 好像沒有 chain 的 class
+    contract: Contract
