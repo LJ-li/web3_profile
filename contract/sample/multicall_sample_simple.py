503afcc4b401d0afa285c2a3e482f5db79aac2ee
LJ Li <ljli@xrex.io>
2023-04-21 13:58:28 +0800

    feat: renew sample

--- contract/sample/multicall_sample_simple.py
+++ contract/sample/multicall_sample_simple.py
@@ -4,13 +4,16 @@ from dotenv import load_dotenv
 from multicall import Call, Multicall
 from web3 import Web3
 
-# assuming you are on kovan
+# 這邊可以用成 enum
 MKR_TOKEN = '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2'
-MKR_WHALE = '0x9a3f63F053512597d486cA679Ce5A0D13b98C8db'
+MKR_WHALE = '0x40f2ec751f922db371b2404049a40d1d5de57ab0'
+MKR_FISH = '0x96294a10222d69d29ca047c0e9e5e707d1d5a2d6'
 
 
-def from_wei(value):
-    return value
+def from_wei(value: int) -> float | None:
+    if value is None:
+        return None
+    return value / 1e18
 
 
 load_dotenv()
@@ -23,9 +26,9 @@ print(w3.eth.chain_id)
 multi = Multicall(
     calls=[
         Call(target=MKR_TOKEN, function=['balanceOf(address)(uint256)', MKR_WHALE], returns=[('whale', from_wei)]),
-        Call(target=MKR_TOKEN, function=['balanceOf(address)(uint256)', MKR_WHALE], returns=[('fish', from_wei)]),
+        Call(target=MKR_TOKEN, function=['balanceOf(address)(uint256)', MKR_FISH], returns=[('fish', from_wei)]),
     ],
     _w3=w3,
 )
-
-print(multi())  # {'whale': 566437.0921992733, 'fish': 7005.0, 'supply': 1000003.1220798912}
+result = await multi()
+print()  # {'whale': 566437.0921992733, 'fish': 7005.0, 'supply': 1000003.1220798912}
